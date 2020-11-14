a1fa4c71c13d59613454c4cdf465ce52b0371c3d
alemi <me@alemi.dev>
2020-11-14 03:24:54 +0100

    Added slowtype, countdown, figlet

--- modules/text.py
+++ modules/text.py
@@ -1,11 +1,63 @@
 import random
 import asyncio
 import subprocess
+import time
 
 from telethon import events
 
-from util import can_react, set_offline
+from util import can_react, set_offline, batchify
 from util.globals import PREFIX
+
+import pyfiglet
+
+FIGLET_FONTS = pyfiglet.FigletFont.getFonts()
+
+# edit message adding characters one at a time
+@events.register(events.NewMessage(
+        pattern=r"{p}(?:slow|sl)(?: |)(?P<timer>-t [0-9.]+|)(?: |)(?P<text>.*)".format(p=PREFIX), outgoing=True))
+async def slowtype(event):
+    if not can_react(event.chat_id):
+        return
+    args = event.pattern_match.groupdict()
+    print(f" [ making text appear slowly ]")
+    interval = 0.5
+    if args["timer"] != "":
+        interval = float(args["timer"].replace("-t ", ""))
+    if args["text"] == "":
+        return 
+    msg = ""
+    for c in args["text"]:
+        msg += c
+        if c.isspace():
+            continue # important because sending same message twice causes an exception
+        t = asyncio.sleep(interval) # does this "start" the coroutine early?
+        await event.message.edit(msg)
+        await t # does this work? I should read asyncio docs
+    await set_offline(event.client)
+
+def interval(delta):
+    if delta > 100:
+        return 5
+    if delta > 20:
+        return 1
+    if delta > 10:
+        return 0.5
+    return 0.25
+
+# edit message adding characters one at a time
+@events.register(events.NewMessage(
+        pattern=r"{p}(?:countdown|cd)(?: |)(?P<timer>[0-9.]+)".format(p=PREFIX)))
+async def countdown(event):
+    if not can_react(event.chat_id):
+        return
+    end = time.time() + float(event.pattern_match.group("timer"))
+    msg = event.raw_text + "\n` → Countdown ` **{:.1f}**"
+    print(f" [ countdown ]")
+    while time.time() < end:
+        await event.message.edit(msg.format(time.time() - end))
+        await asyncio.sleep(interval(end - time.time()))
+    await event.message.edit(msg.format(0))
+    await set_offline(event.client)
 
 # make character random case (lIkE tHiS)
 @events.register(events.NewMessage(
@@ -49,6 +101,34 @@ async def shrug(event):
         await event.message.edit(r'¯\_(ツ)_/¯')
     else:
         await event.reply(r'¯\_(ツ)_/¯')
+    await set_offline(event.client)
+
+# Replace or reply with figlet art
+@events.register(events.NewMessage(
+    pattern=r"{p}figlet(?: |)(?P<list>-l|)(?: |)(?P<font>-f [^ ]+|)(?: |)(?P<text>.*)".format(p=PREFIX)))
+async def figlettext(event):
+    if not can_react(event.chat_id):
+        return
+    print(f" [ figlet ]")
+    args = event.pattern_match.groupdict()
+    if args["list"] == "-l":
+        msg = f"` → ` **Figlet fonts : ({len(FIGLET_FONTS)})\n```[ "
+        msg += " ".join(FIGLET_FONTS)
+        msg += " ]```"
+        for m in batchify(msg, 4090):
+            await event.message.reply(m)
+        return
+    font = "slant"
+    f = args["font"].replace("-f ", "")
+    if f != "" and f in FIGLET_FONTS:
+        font = f
+    if args["text"] == "":
+        return
+    result = pyfiglet.figlet_format(args["text"], font=font)
+    if event.out:
+        await event.message.edit("``` →\n" + result + "```")
+    else:
+        await event.reply("```→\n" + result + "```")
     await set_offline(event.client)
 
 # Run fortune
@@ -117,8 +197,17 @@ class TextModules:
     def __init__(self, client):
         self.helptext = ""
 
+        client.add_event_handler(slowtype)
+        self.helptext += "`→ .slow [-t 0.1] <message> ` makes the message apper slowly *\n"
+
+        client.add_event_handler(countdown)
+        self.helptext += "`→ .cd <time> ` count down time\n"
+
         client.add_event_handler(randomcase)
         self.helptext += "`→ .rc <message> ` maKe mEsSAgEs lIkE tHIs *\n"
+
+        client.add_event_handler(figlettext)
+        self.helptext += "`→ .figlet [-l] [-f font] <text> ` maKe figlet art\n"
 
         client.add_event_handler(shrug)
         self.helptext += "`→ .shrug ` replace or reply with shrug composite emote\n"
