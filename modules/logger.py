59b4e9096af998648a2a1343767443b4e90ffa88
alemi <me@alemi.dev>
2020-11-16 23:07:14 +0100

    oof

--- modules/logger.py
+++ modules/logger.py
@@ -98,7 +98,7 @@ async def dellogger(event):
     entry["original_update"] = entry["original_update"].to_dict()
     entry["WHO"] = "UNKNOWN" # Delete event doesn't tell you who deleted, but msgs are unique
     entry["WHAT"] = "Delete"
-    elif ("channel_id" in entry["original_update"] and 
+    if ("channel_id" in entry["original_update"] and 
             entry["original_update"]["channel_id"] is not None):
         entry["WHERE"] = entry["original_update"]["channel_id"]
     else:
