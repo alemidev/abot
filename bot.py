61196c9bc2d552e9c35ae5ba275ab0a0f1759d8e
alemi <me@alemi.dev>
2020-11-17 20:15:38 +0100

    started porting to pyrogram

--- bot.py
+++ bot.py
@@ -1,90 +1,27 @@
 #!/usr/bin/env python
 """
-This is my take on Telegram userbots. I want this to be scalable so I made 
-'module' classes which register handlers on the main bot. This file 
-just makes the client, instantiates the classes, makes the help command 
-and runs the client. All handlers are defined inside modules/
+WOOOT a pyrogram rewrite im crazyyy
 """
-import json
-import time
-import os
-import sys
 
-from telethon import TelegramClient, events
+from pyrogram import Client
 
-# TODO make a module loader
-from modules.text import TextModules
-from modules.search import SearchModules
-from modules.files import FilesModules
-from modules.meme import MemeModules
-from modules.system import SystemModules
-from modules.trigger import TriggerModules
-from modules.management import ManagementModules
-from modules.bully import BullyModules
-from modules.logger import LoggerModules
-from modules.math import MathModules
 
-from util import set_offline
-from util.permission import is_allowed
+class alemiBot(Client):
+    def __init__(self, name):
+        super().__init__(
+            name,
+            plugins=dict(root=f"plugins/"),
+            workdir="./",
+            app_version="alemibot v0.1",)
 
-import logging
-logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
-                    level=logging.WARNING)
+    async def start(self):
+        await super().start()
+        print("> Bot started")
 
-config = None
+    async def stop(self, *args):
+        await super().stop()
+        print("> Bot stopped")
 
-with open("config.json") as f:
-    config = json.load(f)
-
-client = TelegramClient(
-    config["NAME"], config["ID"],
-    config["HASH"],
-    proxy=None
-)
-
-with client:
-    helptext = "" # lmao don't look at how helptext is handled inside the module classes
-
-    helptext += LoggerModules(client).helptext
-    helptext += SystemModules(client).helptext
-    helptext += SearchModules(client).helptext
-    helptext += TextModules(client).helptext
-    helptext += MathModules(client).helptext
-    helptext += MemeModules(client).helptext
-    helptext += BullyModules(client).helptext
-    helptext += FilesModules(client).helptext
-    helptext += ManagementModules(client).helptext
-    helptext += TriggerModules(client).helptext
-
-    # Help message
-    @events.register(events.NewMessage(pattern=r"\.help"))
-    async def helper(event):
-        if event.out or is_allowed(event.sender_id):
-            await event.reply("` ᚨᛚᛖᛗᛁᛒᛟᛏ v0.1`\n" +
-                              "`→ .help ` print this\n" +
-                                helptext +
-                             f"__Commands with * are available to trusted users__\n" +
-                            "\nhttps://github.com/alemigliardi/alemibot")
-
-    client.add_event_handler(helper)
-
-    # async def edit_last(client):
-    #     lastmsg = (await client.get_messages('me'))[0]
-    #     if lastmsg.message.startswith(".update"):
-    #         await lastmsg.edit(lastmsg.message + " [DONE]")
-    
-    client.loop.run_until_complete(client.send_message('me', '` → ᚨᛚᛖᛗᛁᛒᛟᛏ ` **online**'))
-
-    print()
-    print(' [ Press Ctrl+C to stop this ]\n')
-    client.run_until_disconnected()
-
-print()
-try:
-    print("→ Bot will self restart in 5 seconds, CTRL+C now to stop it")
-    time.sleep(5)
-    print()
-    os.execv(__file__, sys.argv)
-except KeyboardInterrupt as e:
-    print()
-
+if __name__ == "__main__":
+    app = alemiBot("debug")
+    app.run()
---
+++ plugins/text.py
@@ -0,0 +1,233 @@
+import asyncio
+import secrets
+import subprocess
+import time
+import traceback
+
+from collections import Counter
+
+from pyrogram import filters
+
+from util import set_offline, batchify
+# from util.globals import PREFIX
+# from util.parse import cleartermcolor
+# from util.permission import is_allowed
+
+from bot import alemiBot
+
+import pyfiglet
+
+FIGLET_FONTS = pyfiglet.FigletFont.getFonts()
+
+# edit message adding characters one at a time
+@alemiBot.on_message(filters.me & filters.regex(pattern=
+    r"[\.\/](?:sl|slow)(?: |)(?P<timer>-t [0-9.]+|)(?: |)(?P<batch>-b [0-9]+|)(?P<text>.*)"
+))
+async def slowtype(_, message):
+    args = message.matches[0]
+    print(f" [ making text appear slowly ]")
+    interval = 0.5
+    batchsize = 1
+    if args["timer"] != "":
+        interval = float(args["timer"].replace("-t ", ""))
+    if args["batch"] != "":
+        batchsize = int(args["batch"].replace("-b ", ""))
+    if args["text"] == "":
+        return 
+    msg = ""
+    try:
+        for seg in batchify(args["text"], batchsize):
+            msg += seg
+            if seg.isspace() or seg == "":
+                continue # important because sending same message twice causes an exception
+            t = asyncio.sleep(interval) # does this "start" the coroutine early?
+            await message.edit(msg)
+            await t # does this work? I should read asyncio docs
+    except:
+        traceback.print_exc()
+        pass # msg was deleted probably
+    # await set_offline(event.client)
+
+def interval(delta):
+    if delta > 100:
+        return 5
+    if delta > 20:
+        return 1
+    if delta > 10:
+        return 0.5
+    return 0.25
+
+# Display a countdown
+@alemiBot.on_message(filters.command(["countdown", "cd"], prefixes="."))
+async def countdown(_, message):
+    if message.outgoing:
+        tgt_msg = message
+    else:
+        tgt_msg = await message.reply_text("` → `")
+    end = time.time() + 5
+    if len(message.command) > 1:
+        try:
+            end = time.time() + float(message.command[1])
+        except ValueError:
+            return await tgt_msg.edit("`[!] → ` argument must be a float")
+    msg = tgt_msg.text + "\n` → Countdown ` **{:.1f}**"
+    print(f" [ countdown ]")
+    while time.time() < end:
+        await tgt_msg.edit(msg.format(time.time() - end))
+        await asyncio.sleep(interval(end - time.time()))
+    await tgt_msg.edit(msg.format(0))
+
+# make character random case (lIkE tHiS)
+@alemiBot.on_message(filters.me & filters.command(["rc", "randomcase"], prefixes="."))
+async def randomcase(_, message):
+    print(f" [ making message randomly capitalized ]")
+    text = re.sub("[\.\/](?:rc|randomcase)(?: |)", "", message.text)
+    if text == "":
+        return 
+    msg = "" # omg this part is done so badly
+    val = 0  # but I want a kinda imbalanced random
+    upper = False
+    for c in text:
+        last = val
+        val = secrets.randbelow(4)
+        if val > 2:
+            msg += c.upper()
+            upper = True
+        elif val < 1:
+            msg += c
+            upper = False
+        else:
+            if upper:
+                msg += c
+                upper = False
+            else:
+                msg += c.upper()
+                upper = True
+    await message.edit(msg)
+
+# make character random case (lIkE tHiS)
+@alemiBot.on_message(filters.command(["shrug"], prefixes="."))
+async def shrug(_, message):
+    print(f" [ ¯\_(ツ)_/¯ ]")
+    if message.outgoing:
+        await message.edit(r'¯\_(ツ)_/¯')
+    else:
+        await message.reply_text(r'¯\_(ツ)_/¯')
+
+# Replace or reply with figlet art
+@alemiBot.on_message(filters.regex(pattern=
+    r"[\.\/]figlet(?: |)(?:(?P<list>-l)|(?P<font>-f [^ ]+)|(?P<random>-r)|)(?: |)(?P<text>.*)"
+))
+async def figlettext(_, message):
+    print(f" [ figlet ]")
+    args = message.matches[0]
+    if args["list"] == "-l":
+        msg = f"` → ` **Figlet fonts : ({len(FIGLET_FONTS)})\n```[ "
+        msg += " ".join(FIGLET_FONTS)
+        msg += " ]```"
+        for m in batchify(msg, 4090):
+            await event.message.reply_text(m)
+        return
+    font = "slant"
+    if args["random"] == "-r":
+        font = secrets.choice(FIGLET_FONTS)
+    elif args["font"] is not None and args["font"] != "":
+        f = args["font"].replace("-f ", "")
+        if f != "" and f in FIGLET_FONTS:
+            font = f
+    if args["text"] == "":
+        return
+    result = pyfiglet.figlet_format(args["text"], font=font)
+    if message.outgoing:
+        await message.edit("<code> →\n" + result + "</code>", parse_mode='html')
+    else:
+        await message.reply("<code>→\n" + result + "</code>", parse_mode='html')
+
+# Run fortune
+@alemiBot.on_message(filters.command(["fortune"], prefixes="."))
+async def fortune(_, message):
+    try:
+        print(f" [ running command \"fortune\" ]")
+        result = subprocess.run("fortune", capture_output=True)
+        output = cleartermcolor(result.stdout.decode())
+        if message.outgoing:
+            await message.edit(message.text + "\n``` → " + output + "```")
+        else:
+            await message.reply_text("``` → " + output + "```")
+    except Exception as e:
+        await message.reply_text("`[!] → ` " + str(e))
+
+# Roll dice
+# Replace or reply with figlet art
+@alemiBot.on_message(filters.regex(pattern=
+    r"[\.\/](?:random|rand|roll)(?: |)(?:(?:(?P<num>[0-9]+|)d(?P<max>[0-9]+))|(?:(?P<batch>-n [0-9]+|)(?: |)(?P<values>.*)))"
+))
+async def rand(_, message):
+    args = message.matches[0]
+    try:
+        res = []
+        times = 1
+        out = ""
+        if args["num"] not in [ "", None ]:
+            times = int(args["num"])
+        elif args["batch"] not in [ "", None ]:
+            times = int(args["batch"].replace("-n ", ""))
+        if args["max"] not in [ "", None ]: # this checking is kinda lame
+            maxval = int(args["max"])
+            print(f" [ rolling d{maxval} ]")
+            for i in range(times):
+                res.append(secrets.randbelow(maxval) + 1)
+            if times > 1:
+                out += f"`→ Rolled {times}d{maxval}` : **{sum(res)}**\n"
+        elif args["values"] != None and args["values"] != "":
+            choices = args["values"].split(" ")
+            print(f" [ rolling {choices} ]")
+            for i in range(times):
+                res.append(secrets.choice(choices))
+            res_count = Counter(res)
+            if times > 1:
+                out += "`→ Random choice ` **" + res_count.most_common(1)[0][0] + "**\n"
+        else:
+            choices = [ 1, 0 ]
+            print(f" [ rolling {choices} ]")
+            for i in range(times):
+                res.append(secrets.choice(choices))
+            if times > 1:
+                out += "` → Binary " + "".join(str(x) for x in res) + "`\n"
+                res = [] # so it won't do the thing below
+        for r in res:
+            out += f"` → ` **{r}**\n"
+        if message.outgoing:
+            await message.edit(message.text + "\n" + out)
+        else:
+            await message.reply_text(out)
+    except Exception as e:
+        traceback.print_exc()
+        await message.reply_text("`[!] → ` " + str(e))
+
+# class TextModules:
+#     def __init__(self, client):
+#         self.helptext = "`━━┫ TEXT `\n"
+# 
+#         client.add_event_handler(slowtype)
+#         self.helptext += "`→ .slow [-t n] [-b n] <message> ` type msg slowly\n"
+# 
+#         client.add_event_handler(countdown)
+#         self.helptext += "`→ .cd <time> ` count down time *\n"
+# 
+#         client.add_event_handler(randomcase)
+#         self.helptext += "`→ .rc <message> ` maKe mEsSAgEs lIkE tHIs\n"
+# 
+#         client.add_event_handler(figlettext)
+#         self.helptext += "`→ .figlet [-l]|[-r]|[-f font] <text> ` maKe figlet art *\n"
+# 
+#         client.add_event_handler(shrug)
+#         self.helptext += "`→ .shrug ` replace or reply with shrug composite emote *\n"
+# 
+#         client.add_event_handler(fortune)
+#         self.helptext += "`→ .fortune ` you feel lucky!? *\n"
+# 
+#         client.add_event_handler(rand)
+#         self.helptext += "`→ .rand [n]d[max] | [-n <n>] [vals] ` get random stuff *\n"
+# 
+#         print(" [ Registered Text Modules ]")
