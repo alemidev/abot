689efaadd354faf38aa5dee5c0084dd117c7bd97
alemi <me@alemi.dev>
2020-11-18 06:34:24 +0100

    config should be class attr

--- bot.py
+++ bot.py
@@ -7,17 +7,15 @@ from pyrogram import Client
 from configparser import ConfigParser
 
 class alemiBot(Client):
+    config = ConfigParser() # uggh doing it like this is not
+    config.read("config.ini") # super nice but it'll do for now
+
     def __init__(self, name):
         super().__init__(
             name,
             plugins=dict(root=f"plugins/"),
             workdir="./",
             app_version="alemibot v0.1",)
-
-    def load_config(self):
-        super().load_config()
-        self.config = ConfigParser()
-        self.config.read(str(self.config_file))
 
     async def start(self):
         await super().start()
