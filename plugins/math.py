1423e65a7d04729ce4705b943f6c3d225a047288
alemi <me@alemi.dev>
2020-11-18 19:50:11 +0100

    Fixed math modules maybe

--- plugins/math.py
+++ plugins/math.py
@@ -14,6 +14,7 @@ import sympy
 from sympy.solvers import solve
 from sympy.plotting import plot3d, plot3d_parametric_line
 from sympy.parsing.sympy_parser import parse_expr
+from sympy import preview
 from plugins.help import HelpCategory
 
 HELP = HelpCategory("MATH")
@@ -24,7 +25,7 @@ HELP.add_help(["expr", "math"], "convert to LaTeX formula",
 @alemiBot.on_message(is_allowed & filters.command(["expr", "math"], prefixes=".") & filters.regex(
     pattern=r"^.(?:expr|math)(?: |)(?P<opt>-latex|)(?: |)(?P<query>.*)"
 ))
-async def expr(_, message):
+async def expr(client, message):
     try:
         arg = message.matches[0]["query"]
         opt = message.matches[0]["opt"]
@@ -46,7 +47,7 @@ HELP.add_help(["plot", "graph"], "plot provided function",
 @alemiBot.on_message(is_allowed & filters.command(["plot", "graph"], prefixes=".") & filters.regex(
     pattern=r"^.(?:plot|graph)(?: |)(?P<opt>-3d|-par|)(?: |)(?P<query>.*)"
 ))
-async def graph(_, message):
+async def graph(client, message):
     try:
         arg = message.matches[0]["query"]
         opt = message.matches[0]["opt"]
@@ -72,7 +73,7 @@ HELP.add_help("solve", "attempt to solve equation",
 @alemiBot.on_message(is_allowed & filters.command("solve", prefixes=".") & filters.regex(
     pattern=r"^.solve(?: |)(?P<query>.*)"
 ))
-async def solve_cmd(event):
+async def solve_cmd(_, message):
     try:
         arg = message.matches[0]["query"]
         print(f" [ mathifying {arg} ]")
