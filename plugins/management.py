cdcc534f67ab2e0fc3677fd41d6a16385554b577
alemi <me@alemi.dev>
2020-11-18 22:15:00 +0100

    don't allow bots

--- plugins/management.py
+++ plugins/management.py
@@ -84,6 +84,8 @@ async def manage_allowed_cmd(client, message):
         elif len(message.command) > 1 and message.command[1] == "@here" \
         or message.command[1] == "@everyone":
             for u in client.iter_chat_members(message.chat.id):
+                if u.is_bot:
+                    continue
                 users_to_allow.append(u)
         elif len(message.command) > 1:
             user = None
