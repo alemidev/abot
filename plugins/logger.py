e6174c4447522fb366b0508838ec0f31f37cf064
alemi <me@alemi.dev>
2020-11-19 02:45:57 +0100

    uggh bytes

--- plugins/logger.py
+++ plugins/logger.py
@@ -84,9 +84,13 @@ HELP.add_help(["stats", "stat"], "get stats",
 async def stats_cmd(client, message):
     count = EVENTS.count_documents({})
     size = DB.command("dbstats")['totalSize']
-    memesize = subprocess.run(["du", "-b", "data/memes", "|", "cut", "-f1"], capture_output=True).stdout
+    memesize = subprocess.run(
+        ["du", "-b", "data/memes", "|", "cut", "-f1"],
+                        capture_output=True).stdout.decode('utf-8')
     memenumber = len(os.listdir("data/memes"))
-    mediasize = subprocess.run(["du", "-b", "data/scraped_media", "|", "cut", "-f1"], capture_output=True).stdout
+    mediasize = subprocess.run(
+        ["du", "-b", "data/scraped_media", "|", "cut", "-f1"],
+                        capture_output=True).stdout.decode('utf-8')
     medianumber = len(os.listdir("data/scraped_media"))
     uptime = str(datetime.now() - client.start_time)
     await edit_or_reply(message, f"` → ` online for **{uptime}**" +
