bb92cf843345ee89e3d6a13b29ef8797cc713009
alemi <me@alemi.dev>
2021-01-27 13:46:53 +0100

    oops shouldn't have pushed these

--- plugins/onlypolls.py
+++
@@ -1,8 +0,0 @@
-from pyrogram import filters
-
-from bot import alemiBot
-
-@alemiBot.on_message(filters.chat(int(alemiBot.config.get("onlypolls", "chatid", fallback="0"))), group=420)
-async def is_it_a_poll(client, message):
-    if not message.poll or not message.poll.is_anonymous:
-        await message.delete()
