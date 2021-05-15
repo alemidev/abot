2eeac0d1e3c10048808716c8f12fa16bcab82704
alemi <me@alemi.dev>
2021-05-15 03:42:16 +0200

    parse_timedelta is slimmer and accepts negative values (and +)

--- util/time.py
+++ util/time.py
@@ -1,32 +1,28 @@
 import re
 from datetime import timedelta
 
+def _search_value(text, token):
+	val = 0
+	res = re.search(rf"(?P<sign>\+|\-|)(?P<val>[0-9]+)(?: |){token}", text)
+	if res:
+		val = int(res["val"])
+		if res["sign"] == "-":
+			val = -val
+	return val
+
+
 def parse_timedelta(text):
-	"""
+	"""Convert string into timedelta
+
 	Will convert a string to a timedelta object. Will parse by
 	searching numbers with time suffixes in the given string, for
-	example 1h, 12min32s and 2y40d
+	example 1h, 12min32s and 2y40d. Will also match sign (+|-|)
 	"""
-	year = 0
-	res = re.search("(?P<val>[0-9]+)(?: |)y", text)
-	if res:
-		year = int(res["val"])
-	day = 0
-	res = re.search("(?P<val>[0-9]+)(?: |)d", text)
-	if res:
-		day = int(res["val"])
-	hour = 0
-	res = re.search("(?P<val>[0-9]+)(?: |)h", text)
-	if res:
-		hour = int(res["val"])
-	minute = 0
-	res = re.search("(?P<val>[0-9]+)(?: |)m", text)
-	if res:
-		minute = int(res["val"])
-	second = 0
-	res = re.search("(?P<val>[0-9]+)(?: |)s", text)
-	if res:
-		second = int(res["val"])
+	year = _search_value(text, "y")
+	day = _search_value(text, "d")
+	hour = _search_value(text, "h")
+	minute = _search_value(text, "m")
+	second = _search_value(text, "s")
 	return timedelta(days=(day+(year*365)),
 					 hours=hour,
 					 minutes=minute,
